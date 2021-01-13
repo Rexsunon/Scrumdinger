38fee5e21da4c8ef4d510fb141b7d6f6cbca2f44
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-13 15:33:20 +0100

    Created DailyScrum model

---
+++ Models/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Evidence Rex Osikhena on 13/01/2021.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Rex", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Rex"], lengthInMinutes: 5, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Rex", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
+    }
+}
---
+++ Scrumdinger/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Evidence Rex Osikhena on 13/01/2021.
+//
+
+import SwiftUI
