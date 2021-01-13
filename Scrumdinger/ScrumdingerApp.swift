183ba012b3b794be71e6f2c9c37d46415ac6cc22
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-13 04:13:39 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Evidence Osikhena on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
