d64c5c6be4b9ae6b08f519c7391fc0bf177712e4
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-16 15:46:37 +0100

    Draw an Arc Segment

--- Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/MeetingTimerView.swift
@@ -32,6 +32,33 @@ struct MeetingTimerView: View {
     }
 }
 
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+
+    private var degreePerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
+    }
+
+    private var startAngle: Angle {
+        Angle(degrees: degreePerSpeaker * Double(speakerIndex) + 1.0)
+    }
+
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreePerSpeaker - 1.0)
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let diamiter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diamiter / 2.0
+        let center = CGPoint(x: rect.origin.x + rect.size.width / 2.0, y: rect.origin.y + rect.size.height / 2.0)
+
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
+
 struct MeetingTimerView_Previews: PreviewProvider {
     @State static var speakers = [ScrumTimer.Speaker(name: "Rex", isCompleted: true), ScrumTimer.Speaker(name: "Jide", isCompleted: false)]
     static var previews: some View {
